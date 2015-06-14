0e4740d1ad95eaf23dc293ae084f90585cb40334
Jesse Ditson <jesse.ditson@gmail.com>
2015-06-13 19:52:34 -0700

    fix some db config issues

--- app/models/mongo/config.js
+++ app/models/mongo/config.js
@@ -5,13 +5,17 @@ var config = {
   database: dbName,
   host: process.env.DB_HOST || 'localhost',
   port: process.env.DB_PORT || 27017,
+  hosts : [
+    {
+      name: process.env.DB_HOST || 'localhost',
+      port: process.env.DB_PORT || 27017
+    }
+  ]
 }
 
 if (process.env.DB_USER) {
   config.username = process.env.DB_USER
   config.password = process.env.DB_PASSWORD
 }
-
-console.log('connecting to db with config: ', config)
 
 module.exports = config;
